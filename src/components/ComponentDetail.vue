690d71bebd2d96ba35c3739e85d5bcb79c306043
Márton Elek <elek@apache.org>
2018-08-11 22:28:56 +0200

    switch to table layout

--- src/components/ComponentDetail.vue
+++ src/components/ComponentDetail.vue
@@ -2,74 +2,115 @@
     <div class="post">
 
         <div v-if="component" class="content">
-            <h2>{{component.spec.type}} component
-                ({{component.metadata.name}})</h2>
-            <p v-for="item in component.items">{{ item.metadata.name }}
-            <ul>
-                <li v-for="(content,filename) in item.data">
-                    {{filename}}
-                </li>
-            </ul>
-            </p>
+
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">{{component.metadata.namespace}}
+                    </li>
+                    <li class="breadcrumb-item">{{component.metadata.name}}
+                    </li>
+                </ol>
+            </nav>
+
+
             <h3>Services</h3>
-            <div class="card-deck">
-                <div class="card" style="width: 18rem;"
-                     v-for="service in services">
-                    <div class="card-body">
-                        <h5 class="card-title">{{service.metadata.name}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            {{service.spec.type}}</h6>
-                        <p v-for="port in service.spec.ports" class="card-text">
+
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Link</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="service in services">
+                    <td>{{service.metadata.name}}</td>
+                    <td>{{service.spec.type}}</td>
+                    <td><p v-for="port in service.spec.ports"
+                           class="card-text">
                             <a v-bind:href="'/api/v1/namespaces/'+namespace+'/services/'+service.metadata.name+':'+port.name+'/proxy/'">{{port.name}}</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
+                    </p></td>
+                </tr>
+                </tbody>
+            </table>
+
             <h3>Statefulsets</h3>
-            <div class="card-deck">
-                <div class="card" style="width: 18rem;"
-                     v-for="statefulset in statefulsets">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            {{statefulset.metadata.name}}</h5>
-                    </div>
-                </div>
-            </div>
+
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="statefulset in statefulsets">
+                    <td>{{statefulset.metadata.name}}</td>
+                </tr>
+                </tbody>
+            </table>
+
             <h3>Configs</h3>
-            <div class="card-deck">
-                <div class="card" style="width: 18rem;"
-                     v-for="config in configs">
-                    <div class="card-body">
-                        <h5 class="card-title">{{config.metadata.name}}</h5>
+
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Edit</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="config in configs">
+                    <td>{{config.metadata.name}}</td>
+
+                    <td>
                         <router-link :to="'/config/' + config.metadata.name"
                                      class="card-link">Edit
                         </router-link>
-                    </div>
-                </div>
-            </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+
             <h3>Pods</h3>
-            <div class="card-deck">
-                <div class="card"
-                     style="min-width: 18rem; max-width: 18rem; margin: 1em;"
-                     v-for="pod in pods">
-                    <div class="card-body">
-                        <h5 class="card-title">{{pod.metadata.name}}</h5>
-                        <h5 class="card-title">{{pod.status.phase}}</h5>
+
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Logging</th>
+                    <th scope="col">Status page</th>
+                    <th scope="col">Console</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="pod in pods">
+                    <td>{{pod.metadata.name}}</td>
+                    <td>{{pod.status.phase}}</td>
+                    <td>
                         <router-link
                                 :to="'/pod/' + pod.metadata.name + '/logging'"
                                 class="card-link">Logging
                         </router-link>
+                    </td>
+                    <td>
                         <router-link
                                 :to="'/pod/' + pod.metadata.name + '/status'"
                                 class="card-link">Status
                         </router-link>
+                    </td>
+                    <td>
                         <router-link
                                 :to="'/pod/' + pod.metadata.name + '/console'"
                                 class="card-link">Console
                         </router-link>
-                    </div>
-                </div>
-            </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
         </div>
     </div>
 
