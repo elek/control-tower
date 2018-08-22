c96052490aa224f41336d31f54e6af44df108b99
Márton Elek <elek@apache.org>
2018-08-22 21:43:30 +0200

    remove instance specific index from kibana link

--- src/components/PodLogging.vue
+++ src/components/PodLogging.vue
@@ -8,7 +8,7 @@ export default {
   data() {
     return {
       url: "/api/v1/namespaces/logging/services/kibana/proxy/app/kibana#/discover?_g=()&_a=(columns:!(_source),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'033e4840-9345-11e8-bc4b-21753a237035',key:kubernetes.pod_name,negate:!f,params:(query:demom-ozone-scm-0,type:phrase),type:phrase,value:demom-ozone-scm-0),"+
-      "query:(match:(kubernetes.pod_name:(query:"+this.$route.params.name+",type:phrase))))),index:'033e4840-9345-11e8-bc4b-21753a237035',interval:auto,query:(language:lucene,query:''),sort:!('@timestamp',desc))",
+      "query:(match:(kubernetes.pod_name:(query:"+this.$route.params.name+",type:phrase))))),interval:auto,query:(language:lucene,query:''),sort:!('@timestamp',desc))",
     }
   }
 }
