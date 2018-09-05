a991fadd4f66c8aa271ab7183a5d423d536a33e4
Márton Elek <elek@apache.org>
2018-09-05 15:42:13 +0200

    fix direct grafana link

--- src/components/Monitoring.vue
+++ src/components/Monitoring.vue
@@ -10,9 +10,11 @@
             }
         },
         created(){
-            this.url = "/api/v1/namespaces/" + this.$store.state.namespace + "/services/"+this.$store.state.namespace+"-grafana-grafana:web/proxy/dashboards";
+            this.url = "/api/v1/namespaces/" + this.$store.state.namespace + "/services/"+this.$store.state.namespace+"-grafana-grafana:web/proxy";
             if (this.$route.query.page) {
                 this.url += this.$route.query.page;
+            } else {
+                this.url += '/dashboards'
             }
 
         }
